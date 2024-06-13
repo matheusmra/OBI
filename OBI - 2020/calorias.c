3ae8b5e1c687182006b50fe2298c2c008ffa4b30
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-06-13 09:44:41 -0300

    Add files via upload

---
+++ OBI - 2020/calorias.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int E1, E2, E3, X;
+    scanf("%d", &E1);
+    scanf("%d", &E2);
+    scanf("%d", &E3);
+    scanf("%d", &X);
+    if(E2-E1<=X){
+        printf("%d", E2);
+    }else{
+        printf("%d", E3);
+        }
+    }
+
+
