0c218fbc375ebdb0667dd9cb94bc8c9a14cbd9d6
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-06-13 08:35:15 -0300

    Atualizar o carteiro.c

--- OBI - 2014/carteiro.c
+++ OBI - 2014/carteiro.c
@@ -4,25 +4,14 @@
 int main() {
     int N, M, i, j, tempo = 0, posicao_atual = 0;
     int *casas, *encomendas;
-
-    // Leitura do n�mero de casas e encomendas
     scanf("%d %d", &N, &M);
-
-    // Aloca��o din�mica dos vetores
     casas = (int*) malloc(N * sizeof(int));
     encomendas = (int*) malloc(M * sizeof(int));
-
-    // Leitura dos n�meros das casas
     for(i = 0; i < N; i++) {
         scanf("%d", &casas[i]);
     }
-
-    // Leitura dos n�meros das encomendas
     for(i = 0; i < M; i++) {
-        scanf("%d", &encomendas[i]);
-    }
-
-    // Processamento das encomendas
+        scanf("%d", 
     for(i = 0; i < M; i++) {
         for(j = 0; j < N; j++) {
             if(encomendas[i] == casas[j]) {
